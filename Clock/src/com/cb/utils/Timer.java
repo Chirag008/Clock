1226ac30004cdf626cb0780a7d81fc4748cf75dd
Chirag Bansal <chirag.bansal17@gmail.com>
2018-04-21 10:20:56 +0530

    Added a new method for getting current date in required format

--- Clock/src/com/cb/utils/Timer.java
+++ Clock/src/com/cb/utils/Timer.java
@@ -1,9 +1,13 @@
 package com.cb.utils;
 
+import java.text.DateFormat;
 import java.util.Date;
 
 public class Timer {
 	public String getCurrentTime(){
 		return new Date().toString();
+	}
+	public String getCurrentTime(DateFormat df){
+		return df.format(new Date());
 	}
 }
