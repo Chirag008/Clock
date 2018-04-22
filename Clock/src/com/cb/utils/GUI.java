7dee82fe479069d1cd2c2c3075ffcbc91fb61fb6
Chirag Bansal <chirag.bansal17@gmail.com>
2018-04-22 11:35:46 +0530

    Changed size of window to 500

--- Clock/src/com/cb/utils/GUI.java
+++ Clock/src/com/cb/utils/GUI.java
@@ -3,16 +3,8 @@ package com.cb.utils;
 import javax.swing.JFrame;
 
 public class GUI extends JFrame{
-
 	public GUI(){
 		//Initializing the window parameters
-		this.setSize(200,200);
-
-	public static void main(){
-		
+		this.setSize(200,700);
 	}
-	
-
-
-
 }
